20eb8b17a13274b370a36bf62bc4bc4392f610c6
Carlos Araya <carlos.araya@gmail.com>
2014-11-18 23:59:22 -0800

    wiring and refactoring

--- js/app.js
+++ js/app.js
@@ -1,10 +1,2 @@
-document.addEventListener('polymer-ready', function() {
-  var navicon = document.getElementById('navicon');
-  var drawerPanel = document.getElementById('drawerPanel');
-  
-  // Attaches click event listener to navicon to toggle drawerPanel
-  navicon.addEventListener('click', function() {
-    drawerPanel.togglePanel();
-  });
-  
-});
+
+//document.addEventListener('polymer-ready', function () {});
